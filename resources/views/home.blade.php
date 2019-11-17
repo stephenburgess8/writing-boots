152e2fd700a3b5638fc1655a06f0a5282b50705d
Stephen Burgess <stephenburgess8@gmail.com>
2019-11-16 18:10:54 -0600

    feat(UI): Basic positions

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,20 +22,59 @@
                         <div class="wb-form-submit-container">
                             <input class="boots-button-submit" id="wb-form-main-submit" type="submit" value="Save Note">
                         </div>
-                        <div class="wdi-panel-action">
-                            <a class="wdi-panel-action-link" href="#">Delete</a>
-                        </div>
+                        <small><strong>Last Saved</strong><br><time>Nov. 16th, 2019<br>2:10 PM</time></small>
                         <div class="wdi-panel-action">
                             <a class="wdi-panel-action-link" href="#">See Versions</a>
                         </div>
                         <div class="wdi-panel-action">
                             <a class="wdi-panel-action-link" href="#">Preview</a>
                         </div>
+                        <div class="wdi-panel-action">
+                            <a class="wdi-panel-action-link" href="#">Delete</a>
+                        </div>
+                    </div>
+                    <div class="wdi-panel-actions">
+                        <h3 class="wdi-h4 wdi-panel-header">Metadata</h3>
+                        <div class="wdi-panel-body">
+                            <ul>
+                                <li class="wdi-panel-list-item"><strong>Title</strong> 2943101419j</li>
+                                
+                                <li class="wdi-panel-list-item"><strong>Description</strong> Out of respect for human interaction? i don't know. trying to imagine an existence where i do not use any google service at all, including replacements like duckduckgo. not to avoid the company itself, but to avoid 'computerization' as a human brain</li>
+                                <li class="wdi-panel-list-item"><strong>Tags</strong> <a href="/tags/test">Test</a>, <a href="/tags/journal">Journal</a></li>
+                                <li class="wdi-panel-list-item"><strong>Privacy</strong> Only Me</li>
+                            </ul>
+                        </div>
                     </div>
                 </form>
             </div>
-            <div class="boots-card">
-                <div class="boots-card-header">{{ __('Saved Notes') }}</div>
+            <div class="boots-card wdi-card">
+                <div class="boots-card-header">{{ __('Word Counts') }}</div>
+                <div class="boots-card-body">
+                    <div class="wdi-card-content">
+                        <ul>
+                            <li class="wdi-panel-list-item"><strong>Words</strong> 1232</li>
+                            <li class="wdi-panel-list-item"><strong>Characters</strong> 75430</li>
+                            <li class="wdi-panel-list-item"><strong>Sentences</strong> 42</li>
+                            <li class="wdi-panel-list-item"><strong>Paragraphs</strong> 6</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="boots-card wdi-card">
+                <div class="boots-card-header">{{ __('Most Frequent Words') }}</div>
+                <div class="boots-card-body">
+                    <div class="wdi-card-content">
+                        <ul>
+                            <li class="wdi-panel-list-item">1232 <strong>Juniper</strong></li>
+                            <li class="wdi-panel-list-item">78 <strong>Wonderosa</strong></li>
+                            <li class="wdi-panel-list-item">42 <strong>scorpion</strong></li>
+                            <li class="wdi-panel-list-item">6 <strong>waves</strong></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="boots-card wdi-card">
+                <div class="boots-card-header">{{ __('Other Notes') }}</div>
                 <div class="boots-card-body">
                     <div class="boots-mini">
                         <h3 class="boots-mini-header">This is a title</h3>
@@ -49,30 +88,10 @@
                             <p>Here we see another example. That's great, isn't it?</p>
                         </div>
                     </div>
-                    <div class="clear"></div>
                 </div>
             </div>
         </div>
         <div class="wdi-panel-side">
-            <h3 class="wdi-h4 wdi-panel-header">Metadata</h3>
-            <div class="wdi-panel-body">
-                <ul>
-                    <li>Title: 43101419j</li>
-                    <li>Last Saved: 2:10 PM, Nov. 16th, 2019</li>
-                    <li>Description: Out of respect for human interaction? i don't know. trying to imagine an existence where i do not use any google service at all, including replacements like duckduckgo. not to avoid the company itself, but to avoid 'computerization' as a human brain</li>
-                    <li>Tags: <a href="/tags/test">Test</a>, <a href="/tags/journal">Journal</a></li>
-                    <li>Privacy: Only Me</li>
-                </ul>
-            </div>
-            <h3 class="wdi-h4 wdi-panel-header">Word Counts</h3>
-            <div class="wdi-panel-body">
-                <ul>
-                    <li>Words: 1232</li>
-                    <li>Characters: 75430</li>
-                    <li>Sentences: 42</li>
-                    <li>Paragraphs: 6</li>
-                </ul>
-            </div>
         </div>
     </div>
 @endsection
